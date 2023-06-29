7be083cec045cf4f602ec550b3ba71b34ccdf611
Refaat Elabd <RefaatElabd7@gmail.com>
2023-06-29 11:22:32 +0300

    add adapter pattern

---
+++ Adapter/Line.cs
@@ -0,0 +1,14 @@
+﻿namespace Adapter
+{
+    public class Line
+    {
+        public Point Start;
+        public Point End;
+
+        public Line(Point start, Point end)
+        {
+            this.Start = start;
+            this.End = end;
+        }
+    }
+}
