fa2b6a8eb0d883d00988bb77da515cdf37eb151e
Refaat Elabd <RefaatElabd7@gmail.com>
2023-07-01 15:42:37 +0300

    Composite Added

---
+++ Composite/Excercise.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+
+namespace Composite
+{
+    public interface IValueContainer: IEnumerable<int>
+    {
+
+    }
+
+    public class SingleValue : IValueContainer
+    {
+        public int Value;
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            yield return this.Value;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class ManyValues : List<int>, IValueContainer
+    {
+
+    }
+
+    public static class ExtensionMethods
+    {
+        public static int Sum(this IEnumerable<IValueContainer> containers)
+        {
+            int result = 0;
+            foreach (var c in containers)
+                foreach (var i in c)
+                    result += i;
+            return result;
+        }
+    }
+}
