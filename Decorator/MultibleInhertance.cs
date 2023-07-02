03571ffe071b516462302be7a01f601147ce2405
Refaat Elabd <RefaatElabd7@gmail.com>
2023-07-02 05:29:18 +0300

    decorator pattern to add a functionality

---
+++ Decorator/MultibleInhertance.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    public interface IBird
+    {
+        public int Weight { get; set; }
+        void Fly();
+    }
+    public class Bird : IBird
+    {
+        public int Weight { get; set; }
+
+        public void Fly()
+        {
+            Console.WriteLine($"on sky with weight {Weight}");
+        }
+    }
+
+    public interface ILizard
+    {
+        public int Weight { get; set; }
+        void Crawl();
+    }
+
+    public class Lizard : ILizard
+    {
+        public int Weight { get; set; }
+        public void Crawl()
+        {
+            Console.WriteLine($"on the dirt with weight {Weight}");
+        }
+    }
+
+    public class Dragon: IBird, ILizard // no multiple inheritance
+    {
+        private Bird bird = new Bird();
+        private Lizard lizard = new Lizard();
+        int weight;
+        public int Weight { 
+            get => weight;
+            set {
+                weight = value;
+                bird.Weight = value;
+                lizard.Weight = value;
+            } 
+        }
+
+        public void Crawl()
+        {
+            lizard.Crawl();
+        }
+
+        public void Fly()
+        {
+            bird.Fly();
+        }
+    }
+}
---
+++ Decorator/Program.cs
@@ -0,0 +1,19 @@
+﻿using Decorator;
+using System.Text;
+
+//var cb = new CodeBuilder();
+//cb.AppendLine("class Foo")
+//  .AppendLine("{")
+//  .AppendLine("}");
+//Console.WriteLine(cb);
+
+
+CodeBuilder s = "hello ";
+s += "world"; // will work even without op+ in CodeBuilder
+              // why? you figure it out!
+Console.WriteLine(s);
+
+Dragon dragon = new Dragon();
+dragon.Weight = 100;
+dragon.Fly();
+dragon.Crawl();
