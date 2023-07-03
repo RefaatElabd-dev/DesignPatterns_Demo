1e15c1b0021512a3c14d40c3322816818e54dc00
Refaat Elabd <RefaatElabd7@gmail.com>
2023-07-04 00:18:38 +0300

    Proxy Pattern added

---
+++ Proxy/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Proxy;
+
+ICar car = new CarProxy(new Driver(12)); // 22
+car.Drive();
+
+// SoACompositeProxy
+
+var creatures = new Creature[100];
+foreach (var c in creatures)
+{
+    c.X++; // not memory-efficient
+}
+
+var creatures2 = new Creatures(100);
+foreach (Creatures.CreatureProxy c in creatures2)
+{
+    c.X++;
+}
