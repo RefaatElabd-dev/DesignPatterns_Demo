f6a6295111345b4a882d9023da68a84a8df4ac2b
Refaat Elabd <RefaatElabd7@gmail.com>
2023-07-04 10:12:24 +0300

    Chain of Responsibility added

---
+++ ChanOfResponsibility/BrokerChain.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChanOfResponsibility
+{
+    public class Query
+    {
+        public string CreatureName;
+
+        public enum Argument
+        {
+            Attack, Defense
+        }
+
+        public Argument WhatToQuery;
+
+        public int Value; // bidirectional
+
+        public Query(string creatureName, Argument whatToQuery, int value)
+        {
+            CreatureName = creatureName ?? throw new ArgumentNullException(paramName: nameof(creatureName));
+            WhatToQuery = whatToQuery;
+            Value = value;
+        }
+    }
+
+    public class Game // mediator pattern
+    {
+        public event EventHandler<Query> Queries; // effectively a chain
+
+        public void PerformQuery(object sender, Query q)
+        {
+            Queries?.Invoke(sender, q);
+        }
+    }
+
+    public class Creature2
+    {
+        private Game game;
+        public string Name;
+        private int attack, defense;
+
+        public Creature2(Game game, string name, int attack, int defense)
+        {
+            this.game = game ?? throw new ArgumentNullException(paramName: nameof(game));
+            this.Name = name ?? throw new ArgumentNullException(paramName: nameof(name));
+            this.attack = attack;
+            this.defense = defense;
+        }
+
+        public int Attack
+        {
+            get
+            {
+                var q = new Query(Name, Query.Argument.Attack, attack);
+                game.PerformQuery(this, q);
+                return q.Value;
+            }
+        }
+
+        public int Defense
+        {
+            get
+            {
+                var q = new Query(Name, Query.Argument.Defense, defense);
+                game.PerformQuery(this, q);
+                return q.Value;
+            }
+        }
+
+        public override string ToString() // no game
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(attack)}: {Attack}, {nameof(defense)}: {Defense}";
+            //                                                 ^^^^^^^^ using a property    ^^^^^^^^^
+        }
+    }
+
+    public abstract class CreatureModifier2 : IDisposable
+    {
+        protected Game game;
+        protected Creature2 creature;
+
+        protected CreatureModifier2(Game game, Creature2 creature)
+        {
+            this.game = game;
+            this.creature = creature;
+            game.Queries += Handle;
+        }
+
+        protected abstract void Handle(object sender, Query q);
+
+        public void Dispose()
+        {
+            game.Queries -= Handle;
+        }
+    }
+
+    public class DoubleAttackModifier2 : CreatureModifier2
+    {
+        public DoubleAttackModifier2(Game game, Creature2 creature) : base(game, creature)
+        {
+        }
+
+        protected override void Handle(object sender, Query q)
+        {
+            if (q.CreatureName == creature.Name &&
+                q.WhatToQuery == Query.Argument.Attack)
+                q.Value *= 2;
+        }
+    }
+
+    public class IncreaseDefenseModifier2 : CreatureModifier2
+    {
+        public IncreaseDefenseModifier2(Game game, Creature2 creature) : base(game, creature)
+        {
+        }
+
+        protected override void Handle(object sender, Query q)
+        {
+            if (q.CreatureName == creature.Name &&
+                q.WhatToQuery == Query.Argument.Defense)
+                q.Value += 2;
+        }
+    }
+}
---
+++ ChanOfResponsibility/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ChanOfResponsibility;
+
+
+//var goblin = new Creature("Goblin", 2, 2);
+//Console.WriteLine(goblin);
+
+//var root = new CreatureModifier(goblin);
+
+//root.Add(new NoBonusesModifier(goblin));
+
+//Console.WriteLine("Let's double goblin's attack...");
+//root.Add(new DoubleAttackModifier(goblin));
+
+//Console.WriteLine("Let's increase goblin's defense");
+//root.Add(new IncreaseDefenseModifier(goblin));
+
+//// eventually...
+//root.Handle();
+//Console.WriteLine(goblin);
+
+
+var game = new Game();
+var goblin = new Creature2(game, "Strong Goblin", 3, 3);
+Console.WriteLine(goblin);
+
+using (new DoubleAttackModifier2(game, goblin))
+{
+    Console.WriteLine(goblin);
+    using (new IncreaseDefenseModifier2(game, goblin))
+    {
+        Console.WriteLine(goblin);
+    }
+}
+
+Console.WriteLine(goblin);
+
