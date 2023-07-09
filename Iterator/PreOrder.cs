ff97b9d6ee9224cafe3d2f132d3d9da68fc6fed6
Refaat Elabd <RefaatElabd7@gmail.com>
2023-07-09 14:40:13 +0300

    iterator added

---
+++ Iterator/PreOrder.cs
@@ -0,0 +1,51 @@
+﻿namespace Iterator.PreOrder
+{
+    public class Node<T>
+    {
+        public T Value;
+        public Node<T> Left, Right;
+        public Node<T> Parent;
+        public Node<T> Current;
+        private bool yieldedStart;
+
+        public Node(T value) => Value = value;
+
+        public Node(T value, Node<T> left, Node<T> right)
+        {
+            Value = value;
+            Left = left;
+            Right = right;
+            left.Parent = right.Parent = Current = this;
+        }
+
+        public IEnumerator<Node<T>> GetEnumerator()
+        {
+            foreach (var node in PreOrder)
+                yield return node;
+        }
+
+        public IEnumerable<Node<T>> PreOrder
+        {
+            get
+            {
+                IEnumerable<Node<T>> PreOrderTraverse(Node<T> current)
+                {
+                    yield return current;
+                    if (current.Left != null)
+                    {
+                        foreach (var left in PreOrderTraverse(current.Left))
+                            yield return left;
+                    }
+                    if (current.Right != null)
+                    {
+                        foreach (var right in PreOrderTraverse(current.Right))
+                            yield return right;
+                    }
+                }
+
+                foreach (var node in PreOrderTraverse(this))
+                    yield return node;
+            }
+        }
+    }
+}
---
+++ Iterator/Program.cs
@@ -0,0 +1,30 @@
+﻿using Iterator;
+using static System.Console;
+
+//var root = new Node<int>(1,
+//              new Node<int>(2), new Node<int>(3));
+
+//// C++ style
+//var it = new InOrderIterator<int>(root);
+
+//while (it.MoveNext())
+//{
+//    Write(it.Current.Value);
+//    Write(',');
+//}
+//WriteLine();
+
+//// C# style
+//var tree = new BinaryTree<int>(root);
+
+//WriteLine(string.Join(",", tree.NaturalInOrder.Select(x => x.Value)));
+
+//// duck typing!
+//foreach (var node in tree)
+//    WriteLine(node.Value);
+
+var root2 = new Iterator.PreOrder.Node<int>(1,
+              new Iterator.PreOrder.Node<int>(2), new Iterator.PreOrder.Node<int>(3));
+
+foreach (var node in root2)
+     WriteLine(node.Value);
