6202c9682d96ff772f287ec1e0e794fe40d09783
Refaat Elabd <RefaatElabd7@gmail.com>
2023-08-29 04:58:22 +0300

    Mediator And Momento Excercises

---
+++ Mediator/Exercise.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediator
+{
+    public class Participant
+    {
+        public int Value { get; set; }
+        public readonly Mediator mediator;
+
+        public Participant(Mediator mediator)
+        {
+            this.mediator = mediator;
+            mediator.Participants.Add(this);
+        }
+
+        public void Say(int n)
+        {
+            this.mediator.Broadcast(this, n);
+        }
+    }
+
+    public class Mediator
+    {
+        public List<Participant> Participants;
+        public Mediator()
+        {
+            this.Participants = new List<Participant>();
+        }
+        public void Broadcast(Participant self, int value)
+        {
+            foreach (Participant p in Participants)
+            {
+                if (!p.Equals(self))
+                {
+                    p.Value += value;
+                }
+            }
+        }
+    }
+}
---
+++ Momento/Exrcise.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento
+{
+    public class Token
+    {
+        public int Value = 0;
+
+        public Token(int value)
+        {
+            this.Value = value;
+        }
+    }
+
+    public class Memento
+    {
+        public Token Token = null;
+        public Memento(int value)
+        {
+            this.Token = new Token(value);
+        }
+
+        public Memento(Token token)
+        {
+            this.Token = token;
+        }
+    }
+
+    public class TokenMachine
+    {
+        public List<Token> Tokens = new List<Token>();
+
+        public Memento AddToken(int value)
+        {
+            var addedToken = new Token(value);
+            this.Tokens.Add(addedToken);
+            return new Memento(addedToken);
+        }
+
+        public Memento AddToken(Token token)
+        {
+            var addedToken = new Token(token.Value);
+            this.Tokens.Add(addedToken);
+            return new Memento(addedToken);
+        }
+
+        public void Revert(Memento m)
+        {
+            for (int i = this.Tokens.Count - 1; i >= 0; i--)
+            {
+                if (m.Token.Equals(this.Tokens[i])) break;
+
+                this.Tokens.Remove(this.Tokens[i]);
+            }
+        }
+    }
+}
